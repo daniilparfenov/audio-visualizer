f02195ad1a8e2c0296c151d781af512b2c6d88a7
danya <reactive722@gmail.com>
2026-02-12 23:18:07 +0300

    #add .wav file playback

--- include/app_state.h
+++ include/app_state.h
@@ -6,6 +6,11 @@
 typedef struct {
     SDL_Window* window;
     SDL_Renderer* renderer;
+
+    // Sound
+    SDL_AudioStream* stream;
+    Uint8* wav_data;
+    Uint32 wav_data_len;
 } AppState;
 
 #endif  // #ifndef APP_STATE_H
--- src/main.c
+++ src/main.c
@@ -11,10 +11,38 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
     }
     *appstate = state;
 
+    // Initialization of SDL subsystems
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    // Create Window + Renderer
     if (!SDL_CreateWindowAndRenderer("Music Visualizer", 800, 600, 0, &state->window, &state->renderer)) {
         SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
+
+    // Load .wav file
+    const char* wav_path = "assets/wav_sample1.wav";
+    SDL_AudioSpec wav_spec;
+    if (!SDL_LoadWAV(wav_path, &wav_spec, &state->wav_data, &state->wav_data_len)) {
+        SDL_Log("Couldn't load .wav file: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    // Create AudioStream
+    state->stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &wav_spec, NULL, NULL);
+    if (!state->stream) {
+        SDL_Log("Couldn't create audio stream: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    // Put audio into stream
+    SDL_PutAudioStreamData(state->stream, state->wav_data, state->wav_data_len);
+
+    // Resume audio (because SDL starts the device paused)
+    SDL_ResumeAudioStreamDevice(state->stream);
 
     return SDL_APP_CONTINUE;
 }
@@ -55,6 +83,14 @@ SDL_AppResult SDL_AppIterate(void* appstate) {
 void SDL_AppQuit(void* appstate, SDL_AppResult result) {
     AppState* state = (AppState*)appstate;
     if (state) {
+        if (state->stream) {
+            SDL_DestroyAudioStream(state->stream);
+        }
+
+        if (state->wav_data) {
+            SDL_free(state->wav_data);
+        }
+
         SDL_free(state);
     }
 }
