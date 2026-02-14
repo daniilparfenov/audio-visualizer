aeb813f6b0188f93a67a3869e8b9e03efedd25ea
danya <reactive722@gmail.com>
2026-02-14 22:23:36 +0300

    #add exit with escape button

--- src/main.c
+++ src/main.c
@@ -83,6 +83,12 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event) {
         return SDL_APP_SUCCESS;
     }
 
+    if (event->type == SDL_EVENT_KEY_DOWN) {
+        if (event->key.key == SDLK_ESCAPE) {
+            return SDL_APP_SUCCESS;
+        }
+    }
+
     return SDL_APP_CONTINUE;
 }
 
