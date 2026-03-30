71a8c334d0c38ebd756ed75d6d427a20a4b33bd5
danya <reactive722@gmail.com>
2026-03-30 16:03:31 +0300

    Added benchmarks

--- fft/benchmarks/bench_fft.c
+++ fft/benchmarks/bench_fft.c
@@ -1,11 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "fft.h"
 
-#include <stdio.h>
+#define N (1024)
+#define ITERATIONS 100
 
-int main()
-{
+static void generate_dummy_data(Complex* signal, size_t len) {
+    for (size_t i = 0; i < len; ++i) {
+        signal[i].re = (float)rand() / (float)RAND_MAX;
+        signal[i].im = (float)rand() / (float)RAND_MAX;
+    }
+}
 
-    printf("FFT benchmark\n");
+int main() {
+    printf("FFT benchmarks\n");
+    printf("------------------------------\n");
+
+    // Init bufffers
+    Complex* input = malloc(N * sizeof(Complex));
+    if (!input) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return 1;
+    }
+
+    // Generate input data
+    generate_dummy_data(input, N);
+
+    // Benchmark
+    clock_t start_time = clock();
+    for (int i = 0; i < ITERATIONS; ++i) {
+        fft(input, N);
+    }
+    clock_t end_time = clock();
+
+    // Get Benchmark results
+    double total_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+    double time_per_run_ms = (total_time / ITERATIONS) * 1000.0;
+
+    printf("Size (N)         : %d\n", N);
+    printf("Iterations       : %d\n", ITERATIONS);
+    printf("Total time       : %.4f sec\n", total_time);
+    printf("Avg time per FFT : %.4f ms\n", time_per_run_ms);
+
+    // Free buffers
+    free(input);
 
     return 0;
 }
